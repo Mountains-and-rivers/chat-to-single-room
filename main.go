6a22b83046a3189fbf4b30de58c2f40d4164b198
Mountains-and-rivers <sjdykp@163.com>
2019-06-10 23:03:21 +0800

    根据f房间号发送消息

---
+++ main.go
@@ -0,0 +1,45 @@
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+var addr = flag.String("addr", ":8080", "http service address")
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "home.html")
+}
+
+func testHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	http.ServeFile(w, r, "test.html")
+}
+
+func main() {
+	flag.Parse()
+	go h.run()
+	http.HandleFunc("/test", testHome)
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(w, r)
+	})
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
